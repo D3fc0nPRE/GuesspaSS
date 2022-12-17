7cced772c18f99746a9db7ca5d3b0378eb492ded
D3fc0nPRE <120033173+D3fc0nPRE@users.noreply.github.com>
2022-12-17 06:36:23 +0545

    Update GuessTh3passwd.c

--- GuessTh3passwd.c
+++ GuessTh3passwd.c
@@ -11,7 +11,7 @@
       int n_guesses = 0;   
       if (argc != 1) {
       fprintf(stderr, "usage: %s\n", argv[0]);
-       exit(1);
+      exit(1);
     }
      int result; 
      while (n_guesses < MAX_NUM_GUESSES) {
@@ -23,24 +23,24 @@
         printf("wrong attempt try again (attempt %d)\n", n_guesses);
     }
     if (!result && n_guesses == MAX_NUM_GUESSES) {
-   printf("Access Denied :)\n");
+    printf("Access Denied :)\n");
     exit(2);
     }
     printf("success!\n");
     return 0;
 }
    static int check_password() {
-     char guess[10];              
-     int authenticated = 0;   
-      printf("password: ");
-            if (fgets(guess, 50, stdin) == NULL)
-            return authenticated;
-    if (strlen(guess) == 0)
+   char guess[10];              
+   int authenticated = 0;   
+   printf("password: ");
+   if (fgets(guess, 50, stdin) == NULL)
+   return authenticated;
+        if (strlen(guess) == 0)
         return authenticated;
-         if (guess[strlen(guess)-1] != '\n') {   
-                 while ( !feof(stdin) && getchar() != '\n') 
-                 return authenticated;
-    } else {
+        if (guess[strlen(guess)-1] != '\n') {   
+        while ( !feof(stdin) && getchar() != '\n') 
+        return authenticated;
+   } else {
         guess[strlen(guess)-1] = 0;
     }
     if (strcmp(PASSWORD, guess) == 0)
