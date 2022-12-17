2e73204bb125d507072dbe3dc4a7703f3f01fa76
D3fc0nPRE <120033173+D3fc0nPRE@users.noreply.github.com>
2022-12-17 06:39:16 +0545

    Update GuessTh3passwd.c

--- GuessTh3passwd.c
+++ GuessTh3passwd.c
@@ -8,19 +8,20 @@
  const char PASSWORD[] = "Th3corr3ctpassw0rd1h3r3";      
  static int check_password();
  int main(const int argc, const char *argv[]) {
-      int n_guesses = 0;   
-      if (argc != 1) {
-      fprintf(stderr, "usage: %s\n", argv[0]);
-      exit(1);
+
+ int n_guesses = 0;   
+ if (argc != 1) {
+ fprintf(stderr, "usage: %s\n", argv[0]);
+ exit(1);
     }
      int result; 
      while (n_guesses < MAX_NUM_GUESSES) {
      result = check_password();
      n_guesses++;
-        if (result)
-        break;
-        else
-        printf("wrong attempt try again (attempt %d)\n", n_guesses);
+     if (result)
+     break;
+     else
+     printf("wrong attempt try again (attempt %d)\n", n_guesses);
     }
     if (!result && n_guesses == MAX_NUM_GUESSES) {
     printf("Access Denied :)\n");
@@ -28,7 +29,7 @@
     }
     printf("success!\n");
     return 0;
-}
+    }
    static int check_password() {
    char guess[10];              
    int authenticated = 0;   
@@ -47,4 +48,4 @@
     if (strcmp(PASSWORD, guess) == 0)
     authenticated = 1;
     return authenticated;
-}
+    }
